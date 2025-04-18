21cd799600b009ad98b98fd3a35938fc2b978d26
firas hamila <firas.hm.1996@gmail.com>
2025-04-18 16:12:36 +0200

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,14 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/contextStore";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    // console.log("effect");
-    const token = localStorage.getItem("token");
-    if (token == "true") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-  // console.log("test");
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
-    localStorage.setItem("token", "true");
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-    localStorage.removeItem("token");
-  };
-
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        loginHandler: loginHandler,
-        logoutHandler: logoutHandler,
-      }}
-    >
+    <>
       <MainHeader />
       <main>
         {!isLoggedIn && <Login />}
         {isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </>
   );
 }
 
--- src/store/contextStore.js
+++ src/store/contextStore.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
   email: "",
@@ -8,3 +8,35 @@ const AuthContext = createContext({
   logoutHandler: () => {},
 });
 export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    // console.log("effect");
+    const token = localStorage.getItem("token");
+    if (token == "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // console.log("test");
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+    localStorage.setItem("token", "true");
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("token");
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        loginHandler: loginHandler,
+        logoutHandler: logoutHandler,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
